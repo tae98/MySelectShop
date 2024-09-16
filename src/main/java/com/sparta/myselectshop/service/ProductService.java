bfe4d5e0eb8b3a53eae4dbdfe72648a797b77858
tae98 <98rlaxogus@gmail.com>
2024-09-16 18:44:19 +0900

    관심상품 희망 최저가 업데이트 api

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -1,22 +1,38 @@
 package com.sparta.myselectshop.service;
 
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
+    public static final int MIN_MY_PRICE = 100;
 
     public ProductResponseDto createProduct(ProductRequestDto requestDto) {
 
         Product product = productRepository.save(new Product(requestDto));
 
+        return new ProductResponseDto(product);
+    }
+
+    @Transactional
+    public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto) {
+        int myprice = requestDto.getMyprice();
+        if (myprice < MIN_MY_PRICE){
+            throw new IllegalArgumentException("유효 하지 않은 관심 가격 입니다. 최소 " + MIN_MY_PRICE + "원 이상으로 설정해 주세요.");
+        }
+        Product product = productRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 상품을 찾을수 없습니다. ")
+        );
+        product.update(requestDto);
         return new ProductResponseDto(product);
     }
 }
